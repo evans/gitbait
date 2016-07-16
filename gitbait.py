fc6fd17e312e1448d142a7d982781b1bf7590208
Evans Hauser <evanshauser@gmail.com>
2016-07-15 22:24:43 -0700

    printing out commit messages success

--- gitbait.py
+++ gitbait.py
@@ -1,23 +1,63 @@
 from bs4 import BeautifulSoup
 import urllib2
 import json
+import subprocess
 
 page = urllib2.urlopen('http://www.buzzfeed.com/api/v2/feeds/index').read()
 soup = BeautifulSoup(page, 'html.parser')
 
 #print(soup.prettify())
 full = json.loads(page)
+titles = []
 print type(full['big_stories'])
 print type(full['big_stories'][0])
 print full.keys()
-"""
 for article in full['big_stories']:
-    print article['title']
+    title = article['title']
+    if not (title in titles):
+        titles.append(title)
 for article in full['buzzes']:
-    print article['title']
+    title = article['title']
+    if not (title in titles):
+        titles.append(title)
+title_iter = iter(titles)
+
+
+"""
+Untracked files
+git ls-files --others --exclude-standard
+Modified files
+git diff --name-only
 """
 
-git ls-files --others --exclude-standard
-git diff --name-only
+proc = subprocess.Popen(["git ls-files --others --exclude-standard"], stdout=subprocess.PIPE, shell=True)
+(untracked, err) = proc.communicate()
+#print "untracked:", untracked
+proc = subprocess.Popen(["git diff --name-only"], stdout=subprocess.PIPE, shell=True)
+(modified, err) = proc.communicate()
+#print "modified:", modified
+
+
+git_add = "git add "
+git_commit = "git commit -m "
+for fname in untracked.split('\n'):
+    if fname:
+        git_add_cmd = git_add + fname
+        print git_add_cmd
+        try:
+            message = next(title_iter)
+            if message:
+                git_commit_cmd = git_commit + "\"" + message + "\""
+                print git_commit_cmd
+        except StopIteration as e:
+            git_commit_cmd = git_commit + "\"lol jk get rekt\""
+            print git_commit_cmd
+
+
+
+for fname in modified.split('\n'):
+    if fname:
+        git_cmd = git_add + fname
+        print git_cmd
 
 
